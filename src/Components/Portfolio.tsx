a6ee873bc7e77e4ebefbdab7956105b4b4d0cff5
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-08-30 14:41:16 +0200

    add grid

--- src/Components/Portfolio.tsx
+++ src/Components/Portfolio.tsx
@@ -1,9 +1,11 @@
 import {
+  Box,
   Card,
   CardActionArea,
   CardContent,
   CardMedia,
   Container,
+  Grid,
   Typography,
 } from '@mui/material';
 
@@ -25,11 +27,11 @@ const projects = [
     image: './src/assets/thenue.jpeg',
   },
   {
-    title: 'NueAesthetic Webshop',
-    image: './src/assets/nueAst.png',
+    title: 'Haus E-commerce',
+    image: './src/assets/Haus.jpeg',
   },
   {
-    title: 'Task & Project Management App',
+    title: 'Task & Project App',
     image: './src/assets/smarttask.png',
   },
 ];
@@ -37,25 +39,58 @@ const projects = [
 function Portfolio() {
   return (
     <Container>
-      {projects.map((project) => (
-        <Card key={project.title} sx={{ maxWidth: 345 }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="140"
-              image={project.image}
-              alt="Project Image"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {project.title}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      ))}
+      <Box sx={{ width: '100%' }}>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2 }}>
+          {projects.map((project) => (
+            <Grid key={project.title} item xs={12} sm={6} md={4} lg={3}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={project.image}
+                    alt="Project Image"
+                    sx={{
+                      width: '100%',
+                      height: '300px',
+                      objectFit: 'cover',
+                    }}
+                  />
+                  <CardContent sx={{ textAlign: 'center' }}>
+                    <Typography
+                      gutterBottom
+                      component="div"
+                      sx={{ fontFamily: 'Raleway', fontSize: '18px' }}
+                    >
+                      {project.title}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Container>
   );
 }
 
 export default Portfolio;
+
+// const Item = styled(Paper)(({ theme }) => ({
+//   ...theme.typography.subtitle2,
+//   textAlign: 'left',
+//   color: theme.palette.text.primary,
+//   cursor: 'pointer',
+//   boxShadow: 'none',
+//   transition: 'box-shadow 0.3s ease-in-out',
+//   '&:hover': {
+//     boxShadow: '0 .125rem .625rem rgba(0, 0, 0, 0.2)',
+//     backgroundColor: '#FFFFFF',
+//   },
+//   '& img': {
+//     width: '100%',
+//     height: 'auto',
+//     objectFit: 'cover',
+//   },
+// }));
