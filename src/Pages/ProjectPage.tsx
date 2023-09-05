830cdb84d67465ba56278aa257612abcab6b9601
lysmac <calle@lysmask.se>
2023-09-05 17:18:41 +0200

    all three pages now have individual titles, also updates for every team member

--- src/Pages/ProjectPage.tsx
+++ src/Pages/ProjectPage.tsx
@@ -1,11 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import BlackMenu from '../Components/BlackMenu';
+import BlackMobileMenu from '../Components/BlackMobileMenu';
 import Portfolio from '../Components/Portfolio';
 import Footer from '../Components/footer'; // Adjust the path as needed based on your project structure
-import BlackMenu from "../Components/BlackMenu";
-import BlackMobileMenu from "../Components/BlackMobileMenu";
 
 const ProjectPage: React.FC = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    document.title = 'Best Website | Portfolio';
+  }, []);
 
   // Listen for window resize and adjust `isMobile` accordingly
   useEffect(() => {
@@ -18,7 +22,7 @@ const ProjectPage: React.FC = () => {
     };
 
     window.addEventListener('resize', handleResize);
-    
+
     // Cleanup listener on component unmount
     return () => {
       window.removeEventListener('resize', handleResize);
@@ -27,12 +31,14 @@ const ProjectPage: React.FC = () => {
 
   return (
     <>
-      <header style={{
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center'
-}}>
-        {isMobile ? <BlackMobileMenu/> : <BlackMenu />}
+      <header
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        {isMobile ? <BlackMobileMenu /> : <BlackMenu />}
       </header>
       <main>
         <Portfolio />
